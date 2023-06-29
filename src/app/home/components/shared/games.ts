aa16c2f86cb1ea7109941c81c7980c6f60af2ed0
Vinicius Gabriel Prado de Oliveira <vinigpo@gmail.com>
2023-06-29 00:34:15 -0300

    Ajusta layout card e adiciona games

---
+++ src/app/home/components/shared/games.ts
@@ -0,0 +1,50 @@
+export const gameList = [
+  {
+    image: {
+      urlCapa:
+        'https://upload.wikimedia.org/wikipedia/pt/8/84/Battlefield_4_capa.png',
+      alt: 'Battlefield 4',
+      title: 'Battlefield 4',
+    },
+    descriptionCard: 'Jogue agora!',
+    descriptionAndPlataformGame: 'Completo | PS3 | PS4',
+    price: 59.99,
+    tag: 'Digital',
+  },
+  {
+    image: {
+      urlCapa:
+        'https://cdn1.epicgames.com/offer/e97659b501af4e3981d5430dad170911/EGS_HogwartsLegacy_AvalancheSoftware_S2_1200x1600-2bb94423bf1c7e2fca10577d9f2878b9',
+      alt: 'Hogwarts Legacy',
+      title: 'Hogwarts Legacy',
+    },
+    descriptionCard: 'Liberte a magia!',
+    descriptionAndPlataformGame: 'Completo | PS4 | PS5',
+    price: 79.99,
+    tag: 'Deluxe',
+  },
+  {
+    image: {
+      urlCapa:
+        'https://upload.wikimedia.org/wikipedia/pt/5/59/Crash-Bandicoot-4-capa.jpg',
+      alt: 'Crash Bandicoot Its About Time',
+      title: 'Crash Bandicoot Its About Time',
+    },
+    descriptionCard: 'Desafios',
+    descriptionAndPlataformGame: 'Completo | PS4 | PS5',
+    price: 39.99,
+    tag: '',
+  },
+  {
+    image: {
+      urlCapa:
+        'https://upload.wikimedia.org/wikipedia/pt/7/74/Spider-Man_Miles_Morales_capa.png',
+      alt: 'Homem Aranha - Miresmorales',
+      title: 'Homem Aranha - Miresmorales',
+    },
+    descriptionCard: 'Eai, vem?',
+    descriptionAndPlataformGame: 'Completo | PS5',
+    price: 99.99,
+    tag: 'Exclusivo',
+  },
+];
